9c660be78761eec702c8a9ab8ee688ac9ca22161
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 01:21:25 -0500

    added loop with the given thread

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -3,13 +3,15 @@ package com.pluralsight;
 public class ForLoop {
 
 
-    public static void main(String[] args) {
-        int i = 1;
-
+    public static void main(String[] args) throws InterruptedException{
 
         //add for loop
-        for(i += 10) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+            //super cool counts it down pretty cool
+            Thread.sleep(1000);
 
         }
+        System.out.println("LAUNCH!");
     }
 }
