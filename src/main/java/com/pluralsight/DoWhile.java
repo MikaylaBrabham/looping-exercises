ecddb90688532f6623de4034bec8a67ce321c3a1
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 01:10:45 -0500

    added do while loop class

---
+++ src/main/java/com/pluralsight/DoWhile.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+
+        // start counter start
+        int i = 1;
+
+
+        //add do while loop
+        do {
+            System.out.println("I <3 Java!");
+            i++;
+        }
+        while (i <= 5) {
+
+        }
+    }
+}
