2662b49e3d5854ab1449928548bf704a00dfb9ce
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-20 01:14:22 -0500

    added for loop to start

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class ForLoop {
+
+
+    public static void main(String[] args) {
+        int i = 1;
+
+
+        //add for loop
+        for(i += 10) {
+
+        }
+    }
+}
